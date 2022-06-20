86b654b3dfb277b43e4f4055c36a631c0d4f4f1c
Rahul Kumar <rahulkr9622@gmail.com>
2022-06-20 15:23:17 +0530

    Add specific test conditions

--- misaligned.py
+++ misaligned.py
@@ -4,10 +4,18 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            formatted_text = format_text(i, j, major, minor)
+            print_on_console(formatted_text)
+    return (len(major_colors) * len(minor_colors), formatted_text)
 
+def format_text(i, j, major, minor):
+    formatted_text = f'{i * 5 + j} | {major} | {minor}'
+    return formatted_text
 
-result = print_color_map()
+def print_on_console(formatted_text):
+    print(formatted_text)
+
+result, formatted_text = print_color_map()
 assert(result == 25)
+assert('\t' in formatted_text)
 print("All is well (maybe!)\n")
