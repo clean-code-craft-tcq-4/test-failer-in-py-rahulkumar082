61b79efd5d590e14150625549f38300492c50550
Rahul Kumar <rahulkr9622@gmail.com>
2022-06-21 09:46:47 +0530

    Fix alerter stub

--- alerter.py
+++ alerter.py
@@ -1,3 +1,4 @@
+max_network_attempts = 4  # After that fail the network once (kind of real world scenario)
 alert_failure_count = 0
 
 def network_alert_stub(celcius):
@@ -5,7 +6,12 @@ def network_alert_stub(celcius):
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 500
+    global max_network_attempts
+    max_network_attempts -= 1
+    if (max_network_attempts == 0):
+        max_network_attempts = 4
+        return 500
+    return 200
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
@@ -19,8 +25,16 @@ def alert_in_celcius(farenheit):
         alert_failure_count += 0
 
 
-alert_in_celcius(400.5)
-alert_in_celcius(303.6)
+def test_alert():
+        alert_in_celcius(400.5)
+        assert(alert_failure_count == 0)
+        alert_in_celcius(303.6)
+        assert(alert_failure_count == 0)
+        alert_in_celcius(400.5)
+        assert(alert_failure_count == 0)
+        alert_in_celcius(303.6)
+        assert(alert_failure_count > 0) # Bad network reached
+
 print(f'{alert_failure_count} alerts failed.')
-assert(alert_failure_count > 0)
+test_alert()
 print('All is well (maybe!)')
